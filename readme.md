e0e9d8d51ced6e8da650be1aaf9df730b262a52d
julsarik <julien.sarik@finastra.com>
2021-01-01 12:05:08 +0100

    change image name

--- readme.md
+++ readme.md
@@ -1,12 +1,12 @@
 # build image
 ```
-docker build -t pandas:latest .
+docker build -t sklearn:latest .
 ```
 
 # run image
 ````
-docker run -it --rm --name pandas `
+docker run -it --rm --name sklearn `
 -v "C:\Users\julsarik\projects\machine-learning\data:/data" `
 -v "C:\Users\julsarik\projects\machine-learning\scripts:/scripts" `
-pandas bash
+sklearn bash
 ```
--- scripts/decison-trees.py
+++ scripts/decison-trees.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
 
 def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
     model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
@@ -10,6 +11,9 @@ def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
     mae = mean_absolute_error(val_y, preds_val)
     return(mae)
 
+# 
+# Read and explore data
+# 
 # this file contains prices for houses
 melbourne_file_path = '../data/data.csv'
 # read the data and store data in DataFrame titled melbourne_data
@@ -22,48 +26,39 @@ print(melbourne_data.columns)
 print(melbourne_data.head())
 # dropna drops missing values (think of na as "not available")
 melbourne_data = melbourne_data.dropna(axis=0)
-
 # by convention y is the variable name for the prediction target
 y = melbourne_data.Price
-
 # features (columns) are the model input
 melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
 # by convention X is the variable name for the features
 X = melbourne_data[melbourne_features]
 
+# 
+# Decision tree
+# 
 # Define model. Specify a number for random_state to ensure same results each run
 melbourne_model = DecisionTreeRegressor(random_state=1)
 # Fit model
 melbourne_model.fit(X, y)
-
-print("Making predictions for the following 5 houses:")
-print(X.head())
-print("The predictions are")
-print(melbourne_model.predict(X.head()))
-
-# compute MAE
+# compute MAE (Mean Absolute Error)
 predicted_home_prices = melbourne_model.predict(X)
-mae = mean_absolute_error(y, predicted_home_prices)
-print('MAE when validating on training data: ' + str(mae))
-
-
+print('MAE when validating on training data: %f' %mean_absolute_error(y, predicted_home_prices))
 # split data into training and validation data, for both features and target
 # The split is based on a random number generator. Supplying a numeric value to
-# the random_state argument guarantees we get the same split every time we
-# run this script.
+# the random_state argument guarantees we get the same split every time we run this script.
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
-# Define model
-melbourne_model = DecisionTreeRegressor(random_state=1)
 # Fit model
 melbourne_model.fit(train_X, train_y)
 print('with default parameters the decision tree has %d max depth and %d leaves' %(melbourne_model.get_depth(), melbourne_model.get_n_leaves()))
 # get predicted prices on validation data
 val_predictions = melbourne_model.predict(val_X)
+# compute MAE (Mean Absolute Error)
 print('MAE when validating on validation data: %f' %mean_absolute_error(val_y, val_predictions))
 
 # 
 # Customization of the model to avoid underfitting and overfitting
-# 
+#
+# find the best max_leaf_nodes parameter 
 for max_leaf_nodes in [5, 50, 500, 5000]:
     mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
     print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, mae))
@@ -72,11 +67,7 @@ for max_leaf_nodes in [5, 50, 500, 5000]:
 # Random forest
 #
 # The random forest uses many trees, and it makes a prediction by averaging the predictions of each component tree. 
-# It generally has much better predictive accuracy than a single decision tree and it works well with default parameters.
-
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error
-
+# It generally has much better predictive accuracy than a single decision tree and it works well with default parameters (i.e. without customization of the model).
 forest_model = RandomForestRegressor(random_state=1)
 forest_model.fit(train_X, train_y)
 melb_preds = forest_model.predict(val_X)
