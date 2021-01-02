d79620336117c62e8832aef4b9d6e67e6aef8128
julsarik <julien.sarik@finastra.com>
2021-01-02 01:05:26 +0100

    use cross validation

--- scripts/cross-validation.py
+++ scripts/cross-validation.py
@@ -15,17 +15,11 @@ data = data.dropna(subset=['Price'])
 # Separate target from predictors
 y = data.Price
 X = data.drop(['Price'], axis=1)
-# Divide data into training and validation subsets
-X_train_full, X_valid_full, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=0)
 # Select categorical columns with relatively low cardinality (convenient but arbitrary)
-categorical_cols = [cname for cname in X_train_full.columns if X_train_full[cname].nunique() < 10 and 
-                        X_train_full[cname].dtype == "object"] # indicate column has text
+categorical_cols = [cname for cname in X.columns if X[cname].nunique() < 10 and 
+                        X[cname].dtype == "object"] # indicate column has text
 # Select numerical columns
-numerical_cols = [cname for cname in X_train_full.columns if X_train_full[cname].dtype in ['int64', 'float64']]
-# Keep selected columns only
-features = categorical_cols + numerical_cols
-X_train = X_train_full[features].copy()
-X_valid = X_valid_full[features].copy()
+numerical_cols = [cname for cname in X.columns if X[cname].dtype in ['int64', 'float64']]
 
 # 
 # Declare preprocessors
@@ -52,21 +46,26 @@ from sklearn.ensemble import RandomForestRegressor
 model = RandomForestRegressor(n_estimators=100, random_state=0)
 
 # 
-# Create and Evaluate the Pipeline
+# Create the Pipeline
 # 
-# pipeline allows to simplify code using data preprocessing
-# preprocessor is given to the pipeline so that the caller does not have to preprocess the data for training and validation
-# it also allows to easily do cross validation
 from sklearn.metrics import mean_absolute_error
 
 # Bundle preprocessing and modeling code in a pipeline
 my_pipeline = Pipeline(steps=[('preprocessor', preprocessor),
                               ('model', model)
                              ])
-# Preprocessing of training data and fitting of the model 
-my_pipeline.fit(X_train, y_train)
-# Preprocessing of validation data and get predictions
-preds = my_pipeline.predict(X_valid)
-# Evaluate the model
-score = mean_absolute_error(y_valid, preds)
-print('MAE:', score)
+
+# 
+# Evaluate the Pipeline using Cross validation
+# 
+# cross validation consists in doing multiple validation on different training and validation sets
+# this way we have better prediction plus we don't have to bother to split the dataset into training and validation set
+from sklearn.model_selection import cross_val_score
+
+# Multiply by -1 since sklearn calculates *negative* MAE
+scores = -1 * cross_val_score(my_pipeline, X, y,
+                              cv=5,
+                              scoring='neg_mean_absolute_error')
+
+print("MAE scores:", scores)
+print("Average MAE score (across experiments):", scores.mean())
